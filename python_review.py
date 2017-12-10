43d4c2351c3690f93a6fdf005a4da5c57f9495e7
sang <rkooo567@google.com>
2017-12-10 02:16:47 -0800

    final preparation

--- python_review.py
+++ python_review.py
@@ -25,3 +25,41 @@ def generate_subsets():
 subsets = generate_subsets()
 for _ in range(3):
 	print(next(subsets))
+
+def fset():
+	items = lambda x: False
+	def add(y):
+		nonlocal items
+		f = items
+		items = lambda x: f(y) or x == y
+		return f(y)
+	return add, items
+
+def gen_naturals():
+	current = 0
+	while True:
+		yield current
+		current += 1
+gen = gen_naturals()
+
+# combiner that combines two input iterators using a given combination function.
+def combiner(iterable1, iterable2, combiner):
+	while True:
+		first_iter = next(iterable1)
+		second_iter = next(iterable2)
+		yield combiner(first_iter, second_iter)
+
+
+# takes a list of iterators and yields items from all of them in order.
+def gen_all_items(lst):
+	for i in lst:
+		yield from i
+
+# returns all subsets of the positive inteegers from 1 to n.
+def generate_subsets():
+	result = [[]]
+	i = 1
+	while True:
+		yield result
+		result.extend([[i] + element for element in result])
+		i += 1
